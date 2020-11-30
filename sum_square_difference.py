f7c2bd07db39359e4ada260e115e82014eafbc4b
amanda841218 <51956432+amanda841218@users.noreply.github.com>
2020-11-30 22:06:16 +0800

    Add files via upload

--- sum_square_difference.py
+++ sum_square_difference.py
@@ -8,10 +8,19 @@ Created on Mon Nov 30 21:32:09 2020
 #https://projecteuler.net/problem=6
 """
 
+#sol 1
 num = int(input())
 numList = [i for i in range(1, num+ 1)]   
 difArray = map(lambda x: x * (num+x+1)*(num-x)/2, numList)
 ssd = 2*sum(difArray)
+
+sol = ssd
+print(sol)
+
+
+#sol 2
+num = int(input())
+ssd = (num * (num+1) * (2*num+1))/6 - (num*(num+1)/2)
 
 sol = ssd
 print(sol)
